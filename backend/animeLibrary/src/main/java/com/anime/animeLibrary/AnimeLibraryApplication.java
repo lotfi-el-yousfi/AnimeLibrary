4c513468a3e97545ed12c718f91fabb794d80a50
lotfi3003 <51326208+lotfi3003@users.noreply.github.com>
2024-06-19 00:29:39 +0200

    Feature/Setup project

---
+++ backend/animeLibrary/src/main/java/com/anime/animeLibrary/AnimeLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.anime.animeLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeLibraryApplication.class, args);
+	}
+
+}
---
+++ backend/animeLibrary/src/test/java/com/anime/animeLibrary/AnimeLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anime.animeLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
