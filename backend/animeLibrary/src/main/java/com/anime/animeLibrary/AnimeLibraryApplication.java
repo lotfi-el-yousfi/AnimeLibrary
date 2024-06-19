20c146d04503fb94a2cc84219e95225b5973967d
lotfi3003 <51326208+lotfi3003@users.noreply.github.com>
2024-06-19 13:14:00 +0200

    Feature/index file

--- backend/animeLibrary/src/main/java/com/anime/animeLibrary/AnimeLibraryApplication.java
+++ backend/animeLibrary/src/main/java/com/anime/animeLibrary/AnimeLibraryApplication.java
@@ -2,8 +2,10 @@ package com.anime.animeLibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
 public class AnimeLibraryApplication {
 
 	public static void main(String[] args) {
