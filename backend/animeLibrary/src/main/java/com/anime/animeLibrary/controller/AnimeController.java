a094c31576a15f6c3cf12e033e2d71b544d72f94
lotfi el yousfi <51326208+lotfi-el-yousfi@users.noreply.github.com>
2024-06-20 10:39:16 +0200

    Feature/adding search by title & id (#15)
    Co-authored-by: lotfi3003 <51326208+lotfi3003@users.noreply.github.com>

--- backend/animeLibrary/src/main/java/com/anime/animeLibrary/controller/AnimeController.java
+++ backend/animeLibrary/src/main/java/com/anime/animeLibrary/controller/AnimeController.java
@@ -5,8 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.anime.animeLibrary.beans.anime;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 public class AnimeController {
@@ -15,7 +20,34 @@ public class AnimeController {
     public String index(Model model) {
         model.addAttribute(
                 "animeList",
-                anime.getAnimeList ());
+                anime.getAnimeList());
         return "index";
+    }
+
+    @GetMapping("/anime-list/title/{queryTitle}")
+    public String findAnimeByName(@PathVariable(required = false) String queryTitle, Model model) {
+        System.out.println(queryTitle);
+
+        List<anime> foundedAnimes = anime.getAnimeList()
+                .stream()
+                .filter(e -> e.getTitle().toUpperCase().startsWith(queryTitle.toUpperCase()))
+                .collect(Collectors.toList());
+
+        model.addAttribute("animeList", foundedAnimes);
+        return "showAnime";
+    }
+
+    @GetMapping("/anime-list/id/{queryId}")
+    public String findAnimeByid(@PathVariable(required = false) String queryId, Model model) {
+        System.out.println(queryId);
+
+//          Optional<anime> foundedAnime  =  anime.getAnimeList()
+//                .stream()
+//                .filter(e -> e.getId() == Integer.valueOf(queryId))
+//                .findFirst() ;
+
+        System.out.println(anime.getAnimeList().get(Integer.valueOf(queryId)));
+        model.addAttribute("animeList", anime.getAnimeList().get(Integer.valueOf(queryId)));
+        return "showAnime";
     }
 }
